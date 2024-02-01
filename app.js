d9f598cae1bef4d450639bb4037a3d736014fca4
Taciuz <stefano.tacelli@gmail.com>
2024-02-01 22:34:28 +0100

    Fixed 'book read' status and started library design

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 let btn = document.getElementsByClassName("addBookBtn");
 const myModal = document.getElementById("myModal");
-const libraryBook = document.getElementsByClassName("libraryBook");
+const libraryBook = document.getElementById("libraryBook");
 const formSubmit = document.getElementById("formSubmit");
+const openModalBtn = document.getElementById("addBook");
 
 
-// btn.onclick = function() {
-//     myModal.style.display = "block";
-// }
+openModalBtn.onclick = function() {
+    myModal.style.display = "block";
+}
 
 function Book(title, author, pages, isRead){
     this.title = title;
@@ -40,18 +41,46 @@ function createBookCard(book){
     const nPages = document.createElement("p");
     const readBtn = document.createElement("button");
 
-    bookTitle.textContent = `$(book.title)`;
-    bookAuthor.textContent = `$(book.author)`;
+    bookCard.classList.add("bookCard");
+
+    bookTitle.textContent = book.title;
+    bookAuthor.textContent = book.author;
+    nPages.textContent = book.pages;
+
+    if(book.isRead){
+        readBtn.style.backgroundColor = "green";
+        readBtn.innerHTML = "Read!";
+    }   else{
+        readBtn.style.backgroundColor = "red";
+        readBtn.innerHTML = "Not Read!";
+    }
 
     bookCard.appendChild(bookTitle);
     bookCard.appendChild(bookAuthor);
     bookCard.appendChild(nPages);
+    bookCard.appendChild(readBtn);
+    libraryBook.appendChild(bookCard);
+    resetValue();
 }
 
 function updateBookLibrary(){
+    closeModal();
+    resetBookGrid();
     for (let book of myLibrary){
         createBookCard(book);
     }
+}
+
+function resetBookGrid(){
+    libraryBook.innerHTML = "";
+}
+
+function closeModal(){
+    myModal.style.display = "none";
+}
+
+function resetValue(){
+    document.getElementById("formSubmit").reset();
 }
 
 formSubmit.onsubmit = addBook;
